bc75d43d53c9ae8b6a371c9190e64af018e3e325
stella-mumbi <stella.mumbi@student.moringaschool.com>
2024-01-14 20:36:03 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,25 +1,74 @@
-function isPalindrome(word) {
+
+
   // Write your algorithm here
-}
+
 
 /* 
-  Add your pseudocode here
+  algorithmic problem solving
+
+  example
+
+  ------------
+
+  Input: "madam"
+Output: true
+
+Input: "robot"
+Output: false
+
+pseudocode
+
+
+------
+function (str)
+if  letters spell same forwards and backwards -> true
+
+  else return false iterate through every letter to confirm the condition.
+
+  if the data type != string return "only strings allowed"
+
+  no empty strings--no spaces
+
+
+ store characters in a variable
+
+
 */
 
 /*
-  Add written explanation of your solution here
+This a problem solving algorithm that helps you output true if  the string is a palindrome that is it will read  the same afterwards or backwards like `"mom"` or `"racecar".It should return false if this is not met
 */
 
 // You can run `node index.js` to view these console logs
+
+
+  
+function isPalindrome(word) {
+  // Write your algorithm here
+  let reversedWord = word.split("").reverse().join("");
+
+  if (word.toLowerCase() === reversedWord.toLowerCase()) {
+    return true;
+  } else {
+    return false;
+  }
+}
+let word = "Alessia";
+console.log(isPalindrome(word));
+// isPalindrome(Alessia)
+
+
 if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+// add your own custom tests in here
+console.log("true");
+console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+console.log("false");
+console.log("=>", isPalindrome("robot"));
 }
 
 module.exports = isPalindrome;
+
+
